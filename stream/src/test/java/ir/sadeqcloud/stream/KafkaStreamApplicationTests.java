d834172600f410057b6632abfb50c117d3dd705f
sadeq <tsts220g@gmail.com>
2022-04-09 18:17:23 +0430

    stream integration test

--- stream/src/test/java/ir/sadeqcloud/stream/KafkaStreamApplicationTests.java
+++ stream/src/test/java/ir/sadeqcloud/stream/KafkaStreamApplicationTests.java
@@ -1,13 +1,23 @@
 package ir.sadeqcloud.stream;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
-@SpringBootTest
+@SpringBootTest // integration test
+@EmbeddedKafka(topics = {"test"},partitions = 1,brokerProperties = { "listeners=PLAINTEXT://localhost:9093", "port=9093" })
+@PropertySource(value = "classpath:/test.properties")
 class KafkaStreamApplicationTests {
-
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
     @Test
     void contextLoads() {
+        String[] topics={"test"};
+        Assertions.assertArrayEquals(embeddedKafkaBroker.getTopics().toArray(),topics);
     }
 
 }
